ae8c64dd86e295fe28d88840a4ec7815f3cc6d98
Faizan Ali Memon <152072495+Faizan-Ali-Memon@users.noreply.github.com>
2023-12-23 13:31:10 +0000

    container config done

---
+++ 01_basics/03-conversionOperations.js
@@ -0,0 +1,71 @@
+//let score = "22bcd"
+// let score = "22"
+//let score = null
+//let score = undefined
+//let score = true
+/*let score = "Faizan"
+
+console.log(typeof (score));
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber); */
+
+// score = 22 =>  number
+// score = "22bcd" => NaN
+// score = null => number = 0
+// score = undefined => NaN
+// score = true => 1
+// score = Faizan => NaN
+
+//let numberIsLoggedIn = 1
+
+//let numberIsLogged = Boolean(numberIsLoggedIn);
+
+//noconsole.log(numberIsLogged);
+
+// 1 = true ; 0 = false
+// "" = false
+// "Faizan" = true 
+
+// It is also possible to convery a number into string
+
+
+//*************************************Operations************************************************************
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**3);
+
+let str1 = "Faizan"
+let str2 = "Mehmood"
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2)
+console.log(2 + "1")
+console.log("1" + 2 + 2)
+console.log(2 + 2 + "1")
+console.log(2 * 4 + 6 % 9); // It is not a good practice
+
+console.log(true); // It will print true
+console.log(+true); // It will print 1
+
+console.log(false); // It will print false
+console.log(+false); // It will print 0
+console.log(+""); // It will print 0
+
+let num1, num2, num3;
+
+num1m1 = num2 = num3 = 8; // It is not a good practice
+
+let gameCounter = 50;
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
