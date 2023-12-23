72369446e9dd972a5c39fd771f73470863070352
Faizan Ali Memon <152072495+Faizan-Ali-Memon@users.noreply.github.com>
2023-12-23 13:28:03 +0000

    container config done

---
+++ 01_basics/02-datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as new version of javascript
+
+// alert(3+3) // we are using nodeJs not browser
+
+console.log( 3
+    +
+    3) // It is best practice to enhance code readibility
+
+//Some dataTypes of JavaScript
+
+let num = 9
+let name ="Faizan"
+let isLogedIn = true
+console.log("Faizan Ali")
+
+// number -> 2 to power 53
+// bigInt
+// string --> ""
+// boolean --> true/false
+// null --> standalone value
+// undefined
+// symbol --> unique value
+
+console.log(typeof "Faizan Ali")
