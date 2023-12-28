3c677a9076e87c3cea927ca47591a3e00635d5ba
Faizan Ali Memon <152072495+Faizan-Ali-Memon@users.noreply.github.com>
2023-12-28 22:27:32 +0000

    container config done

---
+++ 03_basics/05_IIFE.js
@@ -0,0 +1,11 @@
+// Immediatelty Invoked Function Expressions (IIFE)
+
+// NAMED IIFE
+(function chai(){
+    console.log(`DB Connected`);
+}) ();
+
+// Simple IIFE
+( () =>  {
+    console.log(`Chai aur Code`);
+})()
